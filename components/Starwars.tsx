b3e68989070be377729b761242e0e4fba3162354
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2024-10-22 23:08:19 +0100

    starwars api test

---
+++ components/Starwars.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+
+const StarWars = () => {
+  const [character, setCharacter] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchCharacter();
+  }, []);
+
+  const fetchCharacter = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/people/1/");
+      const data = await response.json();
+      setCharacter(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching Star Wars character:", error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View>
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : character ? (
+        <ThemedText>Character Name: {character.name}</ThemedText>
+      ) : (
+        <ThemedText>Failed to load character</ThemedText>
+      )}
+    </View>
+  );
+};
+
+export default StarWars;
