797761acee8c31a7b6eb03cc44363246c653163f
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2024-10-22 23:27:09 +0100

    start msw in root layout

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-import { registerRootComponent } from "expo";
-import { ExpoRoot } from "expo-router";
-
-export const App = () => {
-  const ctx = require.context("./app");
-
-  return <ExpoRoot context={ctx} />;
-};
-
-async function enableMocking() {
-  if (!__DEV__) {
-    return;
-  }
-
-  await import("./msw.polyfills");
-  const { server } = await import("./mocks/server");
-
-  server.listen();
-}
-
-enableMocking().then(() => {
-  registerRootComponent(App);
-});
+import "fast-text-encoding";
+import "react-native-url-polyfill/auto";
+import "expo-router/entry";
